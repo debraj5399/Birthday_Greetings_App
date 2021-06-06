0a7c4a9b647c2a123224cb8ffb15f8786ee3cf4e
debraj5399 <debrajbanik1999@gmail.com>
2021-06-06 20:13:18 +0530

    Initial commit

---
+++ app/src/main/java/com/debraj/birthdaygreet/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.debraj.birthdaygreet
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun createBirthdayCard(view: View) {
+
+        val text=findViewById<EditText>(R.id.nameInput)
+        val name=text.editableText.toString()
+
+        val intent = Intent(this, BirthdayGreetingActivity::class.java)
+        intent.putExtra(BirthdayGreetingActivity.NAME_EXTRA, name)
+        startActivity(intent)
+
+    }
+}
